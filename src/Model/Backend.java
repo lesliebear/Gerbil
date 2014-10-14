27ec3417381174777476f0b9b66a29dd82c90c72
Amulya7x7 <Amulya7x7@localhost>
2014-10-13 22:56:46 -0400

    Model stuff!! and some changes to contorller

--- src/Model/Backend.java
+++ src/Model/Backend.java
@@ -4,46 +4,155 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-
+/**
+ * Kernel/Standard version of backend. 
+ * The backend contains the way to save information once a user 
+ * exits the program but does not wish to loose his/her work.
+ * In the fancy system, users would be the arraylist<user> 
+ * since each user's games would be stored in their object.
+ * 
+ * */
 public class Backend implements Serializable {
 
-	/*Chose arraylist, but could be hashmap I guess*/
+	/**Array List that holds games of the user */
 	ArrayList<Game> games = new ArrayList<Game>();
 	
+	
+	/**
+	 * Adds a game to the arraylist of the games for the single user (in kernel/standard)
+	 * @param g The game object to add to the array list of games in backend. 
+	 * @return True if successfully added, else false
+	 */
 	public boolean addGame(Game g){
 		 games.add(g);
-		 return false;
+		 return true;
 	}
 	
+	/**
+	 * Deletes a game from the arraylist of the games for the single user (in kernel/standard)
+	 * @param in_name Name for the game given by the user. 
+	 * @return True if successfully deleted, else false
+	 */
 	public boolean deleteGame(String in_name){
-		return false;
+		return true;
 		
 	}
 	
-	public ArrayList<Game> loadSavedGames()throws IOException, ClassNotFoundException {
-		return games; 
-	//creates arraylist of games	
+	/**
+	 * Loads the games arraylist in backend for the single user from serialized file(in kernel/standard)
+	 * @return True if successful, else false 
+	 * @throws IOException If error encountered with the serialized file
+	 * @throws ClassNotFoundException If error encountered with class not found
+	 */
+	public boolean loadSavedGames()throws IOException, ClassNotFoundException {
+		return true; 
+	//creates arraylist of games to store here in backend.	
 	}
 	
-	
-	public void saveGames(ArrayList<Game> games) throws IOException { 
+	/**
+	 * Saves the games array list from backend to a serialized file for the single user (in kernel/standard)
+	 * @throws IOException If error encountered with the serialized file
+	 */
+	public void saveGames() throws IOException { 
 		
 	}
 	
+	/**
+	 * Gets the game from the arraylist of user's games based on the name the user gave for the game
+	 * when he/she saved the game. 
+	 * @param in_name Name of the game the user gave when he/she saved the game
+	 * @return The game the user had saved before if it exists, else return null 
+	 */
 	public Game getGame(String in_name){
 		return null;
-		
 	}
 	
+	/**
+	 * Returns the games arraylist
+	 * @return The array list stored in backend that has all the games. 
+	 */
 	public ArrayList<Game> getGameList(){
 		return this.games;
 	}
 	
+	/**
+	 * Sets the game list to backend. 
+	 * @param games The games list that you want to use in the program
+	 */
 	public void setGameList(ArrayList<Game> games){
 		this.games = games;
 	}
 	
+//////////////////////////////////////FANCY SYSTEM//////////////////////////////////////////////////////////////////////////////////
+	
+	/**Array List that holds games of the user */
+	ArrayList<User> users = new ArrayList<User>();
 	
 	
+	/**
+	 * Adds a user to the arraylist of the users(in Fancy)
+	 * @param u User object to add to the list
+	 * @return True if successful, else false
+	 */
+	public boolean addUser(User u){
+		 users.add(u);
+		 return true;
+	}
+	
+	/**
+	 * Deletes a user from the arraylist of the users(in Fancy) 
+	 * @param u User's name to get user from the arraylist of the users to delete
+	 * @return True if successful, else false
+	 */
+	public boolean deleteUser(String name){
+		return true;
+		
+	}
+	
+	/**
+	 * Loads the users arraylist in backend(in Fancy)
+	 * @return True if successful, else false 
+	 * @throws IOException If error encountered with the serialized file
+	 * @throws ClassNotFoundException If error encountered with class not found
+	 */
+	public boolean loadSavedUsers()throws IOException, ClassNotFoundException {
+		return true; 
+	//creates arraylist of users to store here in backend.	
+	}
+	
+	
+	/**
+	 * Saves the users array list from backend to a serialized file (in Fancy)
+	 * @throws IOException If error encountered with the serialized file
+	 */
+	public void saveUsers() throws IOException { 
+		
+	}
+	
+	/**
+	 * Gets the User from the arraylist of users based on the name the user gave for him/herself
+	 * @param in_name Name of the the user gave when he/she created an account
+	 * @return The user object if named user does not exist, else return null 
+	 */
+	public User getUser(String name){
+		return null;
+	}
+	
+	/**
+	 * Returns the users arraylist
+	 * @return The array list stored in backend that has all the users. 
+	 */
+	public ArrayList<User> getUserList(){
+		return this.users;
+	}
+	
+	/**
+	 * Sets the user list to backend. 
+	 * @param u The user list that you want to use in the program is set in the backend.
+	 */
+	public void setUsersList(ArrayList<User> u){
+		this.users= u;
+	}
+
 	
 }
--- src/Model/Block.java
+++ src/Model/Block.java
@@ -3,10 +3,14 @@ package Model;
 import java.util.ArrayList;
 
 public class Block {
-	int id; //NEED THIS FOR DELETE Since we don't want to 
+	/**Id of the block*/
+	int id; 
+	/**Instructions list in the block*/
 	ArrayList<String> instructions; //all instructions are strings.
 	
-	/*Blocks may have other blocks within them, if selected for deletion, all internal should be deleted too*/
+	/*Blocks may have other blocks within them, if selected for deletion, 
+	 * all internal should be deleted too*/
+	/**Nested instructions go in this adjacency list*/
 	ArrayList<Block> nestedBlocks = new ArrayList<Block>();
 	//the nested blocks is for the adjacency => if statements or nested info uses!!! 
 }
