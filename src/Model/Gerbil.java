860e933ea4e405118c7eb0ba9409f44d944a9484
katnunez <katnunez@localhost>
2014-10-13 21:47:54 -0400

    Merge with 6f64746349f456f1f38397cbc02d3317095e8013

---
+++ src/Model/Gerbil.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Gerbil {
+	/*What is forward for the Gerbil?*/
+	
+	int frontX;  //use this to compare with currX and currY to figure out which direciton gerbil is facing
+	int frontY;
+	
+	int currX; 
+	int currY;
+	
+	Gerbil(){
+		
+	}
+}
+
---
+++ src/View/Instructions.java
@@ -0,0 +1,40 @@
+package View;
+
+/**
+ * This class creates a GUI for the Instruction Screen (Help screen).
+ *
+ */
+public class Instructions {
+
+	/**
+	 * Constructor that creates all necessary GUI components.
+	 * 
+	 */
+	public Instructions() {
+		 
+	}
+
+	/**
+	 * Creates the screen by putting the GUI components together.
+	 * 
+	 */
+	public void CreateScreen() {	
+		
+	}
+
+	/**
+	 * Shows the screen.
+	 * 
+	 */
+	public void show() {
+	
+	}
+	
+	/**
+	 * Hides the screen.
+	 * 
+	 */
+	public void hide() {
+	
+	}
+}
