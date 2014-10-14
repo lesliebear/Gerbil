0590e5331c21bdf3e12ed751d25f87ba1fa0b30f
tpham812 <tpham812@localhost>
2014-10-13 21:22:39 -0400

    delete some methods in view

--- src/View/Finish.java
+++ src/View/Finish.java
@@ -1,17 +1,12 @@
 package View;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
 /**
- * This class creates a GUI for the Finish Screen
+ * This class creates a GUI for the Finish Screen.
  */
 public class Finish implements Screen {
 
 	/**
-	 * Constructor that creates all necessary GUI components
+	 * Constructor that creates all necessary GUI components.
 	 * 
 	 */
 	public Finish() {
@@ -19,7 +14,7 @@ public class Finish implements Screen {
 	}
 
 	/**
-	 * Creates the screen by putting the GUI components together
+	 * Creates the screen by putting the GUI components together.
 	 * 
 	 */
 	public void CreateScreen() {	
@@ -27,7 +22,7 @@ public class Finish implements Screen {
 	}
 
 	/**
-	 * Shows the screen
+	 * Shows the screen.
 	 * 
 	 */
 	public void show() {
@@ -35,56 +30,10 @@ public class Finish implements Screen {
 	}
 	
 	/**
-	 * Hides the screen
+	 * Hides the screen.
 	 * 
 	 */
 	public void hide() {
 	
-	}
-	
-	/**
-	 * This class holds the event handler for the buttons
-	 *
-	 */
-	class ButtonEvent implements ActionListener {
-		
-		public void actionPerformed(ActionEvent arg0) {
-			
-		}
-	}
-	
-	/**
-	 * This class holds the event handler for the panel
-	 * 
-	 */
-	class PanelEvent implements WindowListener {
-
-		public void windowActivated(WindowEvent arg0) {
-			
-		}
-
-		public void windowClosed(WindowEvent arg0) {
-			
-		}
-
-		public void windowClosing(WindowEvent arg0) {
-			
-		}
-
-		public void windowDeactivated(WindowEvent arg0) {
-			
-		}
-
-		public void windowDeiconified(WindowEvent arg0) {
-			
-		}
-
-		public void windowIconified(WindowEvent arg0) {
-			
-		}
-
-		public void windowOpened(WindowEvent arg0) {
-			
-		}	
 	}
 }
--- src/View/Play.java
+++ src/View/Play.java
@@ -1,18 +1,13 @@
 package View;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
 /**
- * This class creates a GUI for the Play screen 
+ * This class creates a GUI for the Play screen.
  *
  */
 public class Play {
 
 	/**
-	 * Constructor that creates all necessary GUI components
+	 * Constructor that creates all necessary GUI components.
 	 * 
 	 */
 	public Play() {
@@ -20,7 +15,7 @@ public class Play {
 	}
 
 	/**
-	 * Creates the screen by putting the GUI components together
+	 * Creates the screen by putting the GUI components together.
 	 * 
 	 */
 	public void CreateScreen() {	
@@ -28,7 +23,7 @@ public class Play {
 	}
 
 	/**
-	 * Shows the screen
+	 * Shows the screen.
 	 * 
 	 */
 	public void show() {
@@ -36,57 +31,10 @@ public class Play {
 	}
 	
 	/**
-	 * Hides the screen
+	 * Hides the screen.
 	 * 
 	 */
 	public void hide() {
 	
 	}
-	
-	/**
-	 * This class holds the event handler for the buttons
-	 *
-	 */
-	class ButtonEvent implements ActionListener {
-		
-		public void actionPerformed(ActionEvent arg0) {
-			
-		}
-	}
-	
-	/**
-	 * This class holds the event handler for the panel
-	 * 
-	 */
-	class PanelEvent implements WindowListener {
-
-		public void windowActivated(WindowEvent arg0) {
-			
-		}
-
-		public void windowClosed(WindowEvent arg0) {
-			
-		}
-
-		public void windowClosing(WindowEvent arg0) {
-			
-		}
-
-		public void windowDeactivated(WindowEvent arg0) {
-			
-		}
-
-		public void windowDeiconified(WindowEvent arg0) {
-			
-		}
-
-		public void windowIconified(WindowEvent arg0) {
-			
-		}
-
-		public void windowOpened(WindowEvent arg0) {
-			
-		}	
-	}
 }
-
