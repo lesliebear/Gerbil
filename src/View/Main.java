9dc31a3630ec85c9f9b63a337c9615f37e5df50b
tpham812 <tpham812@localhost>
2014-11-12 16:12:31 -0500

    Main screen done...kind of

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,20 +1,25 @@
 package View;
  
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
+
+
+
 import javax.imageio.ImageIO;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -38,9 +43,63 @@ public class Main extends Screen {
 	public Main() {
 
 		frame = new JFrame("Gerbil");
-		play = new JButton("Play");
-		instructions = new JButton("Instructions");
-		exit = new JButton("Exit");
+		play = new JButton("Play") {
+			public void paint(Graphics g) {
+				this.setContentAreaFilled(false);
+				this.setBorderPainted(false);
+				Graphics2D g2d = (Graphics2D)g;
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+				g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+				super.paint(g);
+				g2d.setColor(Color.WHITE);
+				g2d.fillRoundRect(0,0,getWidth(),getHeight(),18,18);
+				g2d.setColor(Color.BLACK);
+				g2d.drawRoundRect(0,0,getWidth()-1,getHeight()-1,18,18);
+				FontRenderContext frc = new FontRenderContext(null, false, false);
+				Rectangle2D r = getFont().getStringBounds(getText(), frc);
+				float xMargin = (float)(getWidth()-r.getWidth()) / 2;
+				float yMargin = (float)(getHeight()-getFont().getSize()) / 2;
+				g2d.drawString(getText(),xMargin, (float)getFont().getSize()+yMargin);
+			}
+		};
+		instructions = new JButton("Instructions") {
+			public void paint(Graphics g) {
+				this.setContentAreaFilled(false);
+				this.setBorderPainted(false);
+				Graphics2D g2d = (Graphics2D)g;
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+				g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+				super.paint(g);
+				g2d.setColor(Color.WHITE);
+				g2d.fillRoundRect(0,0,getWidth(),getHeight(),18,18);
+				g2d.setColor(Color.BLACK);
+				g2d.drawRoundRect(0,0,getWidth()-1,getHeight()-1,18,18);
+				FontRenderContext frc = new FontRenderContext(null, false, false);
+				Rectangle2D r = getFont().getStringBounds(getText(), frc);
+				float xMargin = (float)(getWidth()-r.getWidth()) / 2;
+				float yMargin = (float)(getHeight()-getFont().getSize()) / 2;
+				g2d.drawString(getText(),xMargin, (float)getFont().getSize()+yMargin);
+			}
+		};
+		exit = new JButton("Exit") {
+			public void paint(Graphics g) {
+				this.setContentAreaFilled(false);
+				this.setBorderPainted(false);
+				Graphics2D g2d = (Graphics2D)g;
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+				g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+				super.paint(g);
+				g2d.setColor(Color.WHITE);
+				g2d.fillRoundRect(0,0,getWidth(),getHeight(),18,18);
+				g2d.setColor(Color.BLACK);
+				g2d.drawRoundRect(0,0,getWidth()-1,getHeight()-1,18,18);
+				FontRenderContext frc = new FontRenderContext(null, false, false);
+				Rectangle2D r = getFont().getStringBounds(getText(), frc);
+				float xMargin = (float)(getWidth()-r.getWidth()) / 2;
+				float yMargin = (float)(getHeight()-getFont().getSize()) / 2;
+				g2d.drawString(getText(),xMargin, (float)getFont().getSize()+yMargin);
+			}
+		}; 
 		try {
 			image = ImageIO.read(new File("10619975_10204553653617548_7614187758233835955_o.jpg"));
 		} catch (Exception ex) {
@@ -60,17 +119,14 @@ public class Main extends Screen {
 	 * 
 	 */
 	protected void createScreen() {
-
+		
+		addEventListeners(null);
 		Dimension dimension = new Dimension(1024, 668);
-		Dimension buttonDimension = new Dimension(100,100);
-		play.setSize(buttonDimension);
-		play.setMinimumSize(buttonDimension);
-		play.setFont(new Font(null, Font.BOLD,20));
-		instructions.setSize(buttonDimension);
-		instructions.setMinimumSize(buttonDimension);
 		instructions.setFont(new Font(null, Font.BOLD,20));
-		exit.setSize(buttonDimension);
-		exit.setMinimumSize(buttonDimension);
+		play.setBackground(Color.WHITE);
+		play.setFont(new Font(null, Font.BOLD,20));
+		instructions.setBackground(Color.WHITE);	
+		exit.setBackground(Color.WHITE);
 		exit.setFont(new Font(null, Font.BOLD,20));
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
@@ -79,17 +135,17 @@ public class Main extends Screen {
 		c.gridx = 0;
 		c.gridy = 0;
 		c.ipady = 40;
-		c.ipadx = 20;
+		c.ipadx = 70;
 		panel.add(play, c);
-		
 		c.gridx = 1;
 		c.gridy = 0;
 		c.ipady = 40;
+		c.ipadx = 10;
 		panel.add(instructions, c);
 		c.gridx = 2;
 		c.gridy = 0;
 		c.ipady = 40;
-		c.ipadx = 20;
+		c.ipadx = 85;
 		panel.add(exit, c);
 		frame.add(panel);
 		frame.setSize(dimension);
@@ -117,10 +173,36 @@ public class Main extends Screen {
 		frame.setVisible(false);
 	}
 
+	/**
+	 * Enable the screen
+	 */
+	public void enable() {
+		
+		frame.setEnabled(true);
+	}
+
+	/**
+	 * Disable the screen
+	 */
+	public void disable() {
+		
+		frame.setEnabled(false);
+	}
+	
+	/**
+	 * Add event listeners
+	 */
 	public void addEventListeners(ActionListener listener) {
 		
-		play.addActionListener(listener);
-		instructions.addActionListener(listener);
-		exit.addActionListener(listener);
+		play.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("It works");
+				
+			}
+			
+		});
+		//instructions.addActionListener(listener);
+		//exit.addActionListener(listener);
 	}
 }
