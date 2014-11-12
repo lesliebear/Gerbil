fc71a6107c501737939ab1ef008a83b05dfb3a48
katnunez <katnunez@localhost>
2014-11-12 16:28:10 -0500

    saved games add

---
+++ src/View/Instructions.java
@@ -0,0 +1,51 @@
+package View;
+ 
+import java.awt.event.ActionListener;
+
+/**
+ * This class creates a GUI for the Instruction Screen (Help screen).
+ *
+ */
+public class Instructions extends Screen{
+
+	/**
+	 * Constructor that creates all necessary GUI components.
+	 * 
+	 */
+	public Instructions() {
+		 
+	}
+
+	/**
+	 * Creates the screen by putting the GUI components together.
+	 * 
+	 */
+	
+	protected void createScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Shows the screen.
+	 * 
+	 */
+	public void show() {
+	
+	}
+	
+	/**
+	 * Hides the screen.
+	 * 
+	 */
+	public void hide() {
+	
+	}
+
+	public void addEventListeners(ActionListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ src/View/UserFunction.java
@@ -0,0 +1,47 @@
+package View;
+ 
+import java.awt.event.ActionListener;
+
+/**
+ * This class creates a GUI for the Function Screen.
+ *
+ */
+public class UserFunction extends Screen{
+
+	/**
+	 * Constructor that creates all necessary GUI components.
+	 * 
+	 */
+	public UserFunction() {
+		 
+	}
+
+	/**
+	 * Creates the screen by putting the GUI components together.
+	 * 
+	 */
+	protected void createScreen() {	
+		
+	}
+
+	/**
+	 * Shows the screen.
+	 * 
+	 */
+	public void show() {
+	
+	}
+	
+	/**
+	 * Hides the screen.
+	 * 
+	 */
+	public void hide() {
+	
+	}
+
+	public void addEventListeners(ActionListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+}
