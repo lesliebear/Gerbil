225092f58bef80c95a64bcf8d886b6ca564bdf0d
katnunez <katnunez@localhost>
2014-12-01 00:09:24 -0500

    Modified DeleteFunction - Changed title from yellow to white

--- src/Start/StartGerbil.java
+++ src/Start/StartGerbil.java
@@ -13,7 +13,6 @@ public class StartGerbil {
 	 * Main method.
 	 * @param args
 	 */
-	
 	public static void main(String[] args) {
 		//Backend bk = new Backend();
 		//System.out.println(bk.saveGames());
@@ -21,8 +20,8 @@ public class StartGerbil {
 		//Conditionals conditionals = new Conditionals("If");
 		//Play play = new Play();
 		//Conditionals c = new Conditionals("If");
-		//DeleteFunction del = new DeleteFunction();'
-		UserFunction uf = new UserFunction();
+		DeleteFunction del = new DeleteFunction();
+		//UserFunction uf = new UserFunction();
 		//Main main = new Main();
 		//Grid g = new Grid(17,17);		
 	}
