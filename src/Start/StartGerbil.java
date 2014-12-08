68d8e07a23db39ae94ca735746f1bbb7cbdf71c8
Amulya7x7 <Amulya7x7@localhost>
2014-12-08 18:30:48 -0500

    removed 'n' and 'g' return of create blocks. and else if and else work as well as function stuff. need to keep testing else if and else stuf... but inserting something without else works (by not inserting)

--- src/Start/StartGerbil.java
+++ src/Start/StartGerbil.java
@@ -19,8 +19,11 @@ public class StartGerbil {
 		Controller controller = new Controller();
 		controller.createBlocks(0, 1, 0, null); //eat
 		controller.createBlocks('e', 1, 1, null);
-		
-		controller.createBlocks(3, 1, 0, null); //if theres no wall
+		controller.createBlocks(5, 2, 0, null); //add an else without if
+		controller.createBlocks('e', 2, 3, null);
+		controller.createBlocks(1, 1, 0, null); //turnleft
+		controller.createBlocks('e', 1, 1, null);
+		/**controller.createBlocks(3, 1, 0, null); //if theres no wall
 		controller.createBlocks(2, 3, 0, null); //move
 		controller.createBlocks('e', 3, 1, null);
 		controller.createBlocks('e', 1, 4, "There'sNoWall");
@@ -30,7 +33,7 @@ public class StartGerbil {
 		controller.createBlocks('e', 5, 4, "There'sWall?"); //else if inserted!!
 		
 		controller.createBlocks(1, 0, 0, null); //insert turn left at line 0
-		controller.createBlocks('e', 0, 1, null);
+		controller.createBlocks('e', 0, 1, null);*/
 		controller.printBlocks(0,controller.getCurrGame().getBlocks());
 		//System.out.println("_______________deleteblockstuff____________________________");
 		//System.out.println(controller.deleteBlock(0));
@@ -54,4 +57,3 @@ public class StartGerbil {
 
 	}
 }
-
