d71b9f604c8ac0da965eb0ae5ef91acbdada4a9e
Amulya7x7 <Amulya7x7@localhost>
2014-12-12 04:56:06 -0500

    conditionals stuff working now

--- src/Control/ActionListenersControl.java
+++ src/Control/ActionListenersControl.java
@@ -709,6 +709,12 @@ public class ActionListenersControl {
 
 				Start.StartGerbil.controller.createBlocks(2,lineSelect,1, null);
 				Start.StartGerbil.controller.createBlocks('e',lineSelect,1,null);
+				System.out.println("MOVE AHEAD BUTTON CLICKED SO PRINT NOW!!!");
+				String[] temp = Start.StartGerbil.controller.getUnFinIns();
+				for(int i =0 ;i<temp.length;i++){
+					System.out.println(temp[i]);
+				}
+				System.out.println("MOVE AHEAD BUTTON CLICKED SO PRINT DONE!!!!!");
 				conditionals.refreshConditionalsJList(Start.StartGerbil.controller.getUnFinIns());
 			}	
 		});
